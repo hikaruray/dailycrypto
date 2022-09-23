d1f01ed202684aba706afcca725b12641f426441
Yasushi Nishihira <51431582+hikaruray@users.noreply.github.com>
2022-09-23 20:02:10 +0900

    Update linebot.py

--- linebot.py
+++ linebot.py
@@ -15,20 +15,20 @@ info = json.load(file)
 CHANNEL_ACCESS_TOKEN = info['CHANNEL_ACCESS_TOKEN']
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
-url='https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
-headers = {
- 
-}
-parameters={
-    'symbol': 'XRP,LTC'
-}
+endPoint = 'https://api.coin.z.com/public'
+path     = '/v1/ticker'
+
 
 def main():
-    response=requests.get(url,headers=headers,params=parameters)
-    data = json.loads(response.text)
-    xrp_price_now = data['data']['XRP']['quote']['USD']['price']
-    ltc_now = data['data']['LTC']['quote']['USD']['price']
-    post=f'The price of xrp is USD{xrp_price_now:.3f}.\n The price of ltc is USD{ltc_now:.3f}.\n See you in the next post'
+    response = requests.get(endPoint + path)
+    data = response.json()
+    new = data['data']
+    currencyName = new[4]['symbol']
+    price1 = new[4]['ask']
+    currencyName2 = new[11]['symbol']
+    price2 = new[11]['ask']
+
+    post=f"The price of {currencyName} is {price1}yen\nThe price of {currencyName2} is {price2}yen"
     USER_ID = info['USER_ID']
     messages = TextSendMessage(text=post)
     line_bot_api.push_message(USER_ID, messages=messages)
